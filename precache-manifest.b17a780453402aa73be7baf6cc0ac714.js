a07bb90596ddabfcece40f22c6861a58d7812844
Matan Ryngler <matanryngler@Matans-MacBook-Pro.local>
2023-06-17 21:34:59 +0300

    Updates

---
+++ precache-manifest.b17a780453402aa73be7baf6cc0ac714.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ba47017db54dd1a9205",
+    "url": "/landing-page/static/css/main.ae4ce00c.chunk.css"
+  },
+  {
+    "revision": "6ba47017db54dd1a9205",
+    "url": "/landing-page/static/js/main.6ba47017.chunk.js"
+  },
+  {
+    "revision": "205849d973eaa941ab0f",
+    "url": "/landing-page/static/js/1.205849d9.chunk.js"
+  },
+  {
+    "revision": "9bd28bf6698dda887d6d",
+    "url": "/landing-page/static/css/2.ac96b4a0.chunk.css"
+  },
+  {
+    "revision": "9bd28bf6698dda887d6d",
+    "url": "/landing-page/static/js/2.9bd28bf6.chunk.js"
+  },
+  {
+    "revision": "1d3bfe9c866fe3e79fea",
+    "url": "/landing-page/static/js/runtime~main.1d3bfe9c.js"
+  },
+  {
+    "revision": "119b35034d160a441bc7ccb4886c224a",
+    "url": "/landing-page/static/media/profilephoto.119b3503.png"
+  },
+  {
+    "revision": "8c05e05ad367280bdfc4e7cc78d8e0a0",
+    "url": "/landing-page/index.html"
+  }
+];
